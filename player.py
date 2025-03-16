ff74922cf4d4074423d1ee4bad9f23b11fecea1e
ravitejamannam <ravitejamannam88@gmail.com>
2025-03-17 03:31:48 +0530

    Added player rotation and movement logic

--- player.py
+++ player.py
@@ -7,6 +7,18 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+    
+    
+    
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
 
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
